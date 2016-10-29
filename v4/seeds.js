a9222e273c313df0569935da284cf0b79e6fdedf
Anthony Benites <anthony.benites17@gmail.com>
2016-10-29 16:08:03 +0000

    comment styling

--- v4/seeds.js
+++ v4/seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Cloud's Rest", 
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        description: "blah blah blah"
+        description: "VHS poutine etsy shoreditch 3 wolf moon. Tbh godard pork belly XOXO vaporware portland, fingerstache brunch crucifix. Narwhal leggings master cleanse, organic mixtape artisan tacos street art pabst quinoa 90's edison bulb. Truffaut ethical intelligentsia salvia leggings, cardigan williamsburg thundercats la croix ennui. Kinfolk kickstarter typewriter tousled glossier, taxidermy coloring book brunch pug fanny pack mixtape fingerstache four loko synth. Synth tilde banh mi, yuccie typewriter brooklyn pitchfork lyft fixie microdosing direct trade viral ugh seitan slow-carb. Disrupt fashion axe fingerstache, forage retro helvetica austin messenger bag echo park woke small batch single-origin coffee meggings bushwick."
     },
     {
         name: "Desert Mesa", 
         image: "https://farm4.staticflickr.com/3859/15123592300_6eecab209b.jpg",
-        description: "blah blah blah"
+        description: "VHS poutine etsy shoreditch 3 wolf moon. Tbh godard pork belly XOXO vaporware portland, fingerstache brunch crucifix. Narwhal leggings master cleanse, organic mixtape artisan tacos street art pabst quinoa 90's edison bulb. Truffaut ethical intelligentsia salvia leggings, cardigan williamsburg thundercats la croix ennui. Kinfolk kickstarter typewriter tousled glossier, taxidermy coloring book brunch pug fanny pack mixtape fingerstache four loko synth. Synth tilde banh mi, yuccie typewriter brooklyn pitchfork lyft fixie microdosing direct trade viral ugh seitan slow-carb. Disrupt fashion axe fingerstache, forage retro helvetica austin messenger bag echo park woke small batch single-origin coffee meggings bushwick."
     },
     {
         name: "Canyon Floor", 
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        description: "blah blah blah"
+        description: "VHS poutine etsy shoreditch 3 wolf moon. Tbh godard pork belly XOXO vaporware portland, fingerstache brunch crucifix. Narwhal leggings master cleanse, organic mixtape artisan tacos street art pabst quinoa 90's edison bulb. Truffaut ethical intelligentsia salvia leggings, cardigan williamsburg thundercats la croix ennui. Kinfolk kickstarter typewriter tousled glossier, taxidermy coloring book brunch pug fanny pack mixtape fingerstache four loko synth. Synth tilde banh mi, yuccie typewriter brooklyn pitchfork lyft fixie microdosing direct trade viral ugh seitan slow-carb. Disrupt fashion axe fingerstache, forage retro helvetica austin messenger bag echo park woke small batch single-origin coffee meggings bushwick."
     }
 ]
 
